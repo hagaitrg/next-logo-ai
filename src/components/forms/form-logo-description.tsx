41dc7bf301da6139efe8092cd53e36269caacf34
hagaitrg <disahagait@gmail.com>
2025-02-08 18:05:44 +0700

    feat : adding from logo description

---
+++ src/components/forms/form-logo-description.tsx
@@ -0,0 +1,103 @@
+"use client";
+
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { useContext } from "react";
+import { FormLogoContext } from "./context/form-logo-context";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "../ui/card";
+import { ArrowLeft, ArrowRight } from "lucide-react";
+import { Textarea } from "../ui/textarea";
+
+const formSchema = z.object({
+  description: z
+    .string({ message: "Description is Required" })
+    .min(3, { message: "min. 3 character" }),
+});
+
+type FormSchemaType = z.infer<typeof formSchema>;
+
+export const FormLogoDescription = () => {
+  const formLogoCtx = useContext(FormLogoContext);
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      description: "",
+    },
+  });
+
+  function onSubmit(values: FormSchemaType) {
+    formLogoCtx.setState({
+      name: "description", //Todo
+      values: { ...formLogoCtx.values, name: values.description },
+    });
+  }
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle className="font-black text-4xl">
+          <h2>Define Your Vision</h2>
+          <CardDescription className="font-semibold text-lg">
+            Share your ideas and inspirations—our AI will craft the perfect logo
+            that aligns with your brand's identity.
+          </CardDescription>
+        </CardTitle>
+      </CardHeader>
+      <CardContent>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <FormField
+              control={form.control}
+              name="description"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Textarea
+                      placeholder="Ex. Desaku - A platform dedicated to empowering villages by connecting communities, promoting local culture, and enhancing rural development through technology."
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className="flex justify-between">
+              <Button
+                variant="outline"
+                type="button"
+                className="px-5"
+                onClick={() => formLogoCtx.setState({ name: "name" })}
+              >
+                <ArrowLeft />
+                Previous
+              </Button>
+              <Button
+                type="submit"
+                className="px-5"
+                disabled={!form.formState.isValid}
+              >
+                Next
+                <ArrowRight />
+              </Button>
+            </div>
+          </form>
+        </Form>
+      </CardContent>
+    </Card>
+  );
+};
--- src/components/forms/form-logo-main.tsx
+++ src/components/forms/form-logo-main.tsx
@@ -3,6 +3,7 @@
 import { useContext, useState } from "react";
 import { FormLogoContext, FormLogoState } from "./context/form-logo-context";
 import { FormLogoName } from "./form-logo-name";
+import { FormLogoDescription } from "./form-logo-description";
 
 const FormStateComponent = () => {
   const formLogoContext = useContext(FormLogoContext);
@@ -10,7 +11,7 @@ const FormStateComponent = () => {
     case "name":
       return <FormLogoName/>
     case "description":
-      return <div>Descirption</div>
+      return <FormLogoDescription/>
     default:
       return <div>Default</div>;
   }
